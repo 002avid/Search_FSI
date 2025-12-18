d9d73a109972639ea45efc4510460ef5bc0ff74b
002avid <adbugyf@gmail.com>
2025-12-18 09:01:46 +0000

    Solved Problem

---
+++ run.py
@@ -0,0 +1,23 @@
+# Search methods
+
+import search
+from utils import branch_and_bound_underestimation_class
+
+ab = search.GPSProblem('A', 'B', search.romania)
+oe = search.GPSProblem('O', 'E', search.romania)
+
+print("BFS")
+print(search.breadth_first_graph_search(ab).path())
+print("-------------------")
+print("DFS")
+print(search.depth_first_graph_search(ab).path())
+print("-------------------")
+print("B&B")
+print(search.branch_and_bound(ab).path())
+print("-------------------")
+print("B&B with underestimation")
+print(search.branch_and_bound_underestimation(ab).path())
+
+# Result:
+# [<Node B>, <Node P>, <Node R>, <Node S>, <Node A>] : 101 + 97 + 80 + 140 = 418
+# [<Node B>, <Node F>, <Node S>, <Node A>] : 211 + 99 + 140 = 450
